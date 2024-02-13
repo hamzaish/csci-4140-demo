ed1a7a709329a3d139876a70e0ccd55c230c5691
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-13 22:09:00 +0800

    Added semi-colon

--- web/photos.php
+++ web/photos.php
@@ -12,8 +12,8 @@
         if(!isset($_COOKIE["username"])){
             header("Location: login.php");
         }
-        $username = $_COOKIE['username']
-        echo "<p>You are logged in as $username</p>"
+        $username = $_COOKIE['username'];
+        echo "<p>You are logged in as $username</p>";
       ?>
       <form action="logout.php" method="post"> 
         <input type="submit" value="Logout"> 
@@ -21,6 +21,9 @@
     </div>
     <div>
         <link href="./index.css" rel="stylesheet" />
+        <form action="upload-photo.php" method="post"> 
+            <input type="submit" value="Upload a Photo"> 
+        </form>
     </div>
   </body>
 </html>
