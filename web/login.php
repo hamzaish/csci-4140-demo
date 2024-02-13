0b6ed346a0f945cd111b6e50830a2128cb410084
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-13 18:22:54 +0800

    Update login.php

--- web/login.php
+++ web/login.php
@@ -4,14 +4,14 @@
         $username = $_POST['username'];
         $pass = $_POST['password'];
         setcookie('username', $username, time()+3600);
-        $sql = "SELECT FROM myusers WHERE name=$username AND passwords=$pass"
-        $result = $conn->query($sql)
-        $rows = pg_num_rows($result)
+        $sql = "SELECT FROM myusers WHERE name=$username AND passwords=$pass";
+        $result = $conn->query($sql);
+        $rows = pg_num_rows($result);
         if($rows == 1){
-            header("Location: https://www.postgresql.org/docs/current/tutorial-accessdb.html", true, 301)
+            header("Location: https://www.postgresql.org/docs/current/tutorial-accessdb.html", true, 301);
         }
         else{
-            header("Location: https://www.postgresql.org", true, 301)
+            header("Location: https://www.postgresql.org", true, 301);
         }
     }
     if(isset($_COOKIE['username'])){
