11bce9853ddd8830f10be64858e7e3e99d9ca245
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-13 18:20:53 +0800

    Added login.php

--- web/login.php
+++ web/login.php
@@ -1,7 +1,20 @@
 <?php
-
-if(isset($_COOKIE['username'])){
-    echo '<p>You are logged in!<p>';
-}
-
+    include('db_connect');
+    if(isset($_POST['button'])){
+        $username = $_POST['username'];
+        $pass = $_POST['password'];
+        setcookie('username', $username, time()+3600);
+        $sql = "SELECT FROM myusers WHERE name=$username AND passwords=$pass"
+        $result = $conn->query($sql)
+        $rows = pg_num_rows($result)
+        if($rows == 1){
+            header("Location: https://www.postgresql.org/docs/current/tutorial-accessdb.html", true, 301)
+        }
+        else{
+            header("Location: https://www.postgresql.org", true, 301)
+        }
+    }
+    if(isset($_COOKIE['username'])){
+        echo '<p>You are logged in!<p>';
+    }
 ?>
