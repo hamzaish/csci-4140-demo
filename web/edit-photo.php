ca3d347e443287a2a52f00ab100480785c72bee5
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-14 10:06:47 +0800

    Update edit-photo.php

--- web/edit-photo.php
+++ web/edit-photo.php
@@ -11,24 +11,19 @@
         // Check image using getimagesize function and get size
         // if a valid number is got then uploaded file is an image
         if (isset($_FILES["image"])) {
-            // directory name to store the uploaded image files
-            // this should have sufficient read/write/execute permissions
-            // if not already exists, please create it in the root of the
-            // project folder
             $targetDir = "uploads/";
             $targetFile = $targetDir . basename($_FILES["image"]["name"]);
             $uploadOk = 1;
             $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-            
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+                echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
             // Validation here
         }
     }
 ?>
-
-<form action="" method="post" enctype="multipart/form-data">
-  <input type="file" id="image" name="filename">
-  <input type="submit" name="submit" value="Upload Photo">
-</form>
 
 </body>
 </html>
