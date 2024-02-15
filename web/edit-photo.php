c6e3a1190a28cdc5524859b03efb057eb3a68592
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-15 21:25:33 +0700

    Update edit-photo.php

--- web/edit-photo.php
+++ web/edit-photo.php
@@ -16,9 +16,9 @@
             $uploadOk = 1;
             $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
             if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-                echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.";
+                echo "<p>The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.<p>";
             } else {
-                echo "Sorry, there was an error uploading your file.";
+                echo "<p>Sorry, there was an error uploading your file.<p>";
             }
             // Validation here
         }
