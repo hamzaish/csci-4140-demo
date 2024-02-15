b70847502f4f6508c6399257b7e37a9ccc4720b5
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-15 23:06:30 +0700

    Temp

--- web/edit-photo.php
+++ web/edit-photo.php
@@ -9,9 +9,10 @@
     if (isset($_POST["submit"])) {
         $file_name = $_FILES["image"]["name"];
         $temp_name = $_FILES["image"]["tmp_name"];
-        $folder = "image/$file_name";
+        $folder = "images/$file_name";
+        move_uploaded_file($temp_name, $folder)
+        echo "<img src = $folder>"
         echo "<p>$file_name<p>";
-
     }
 ?>
 
--- web/photos.php
+++ web/photos.php
@@ -28,14 +28,5 @@
             <input type="submit" name="submit" value="Upload Photo">
         </form>
     </div>
-    <?php
-    if (isset($_POST["submit"])) {
-        $file_name = $_FILES["image"]["name"];
-        $temp_name = $_FILES["image"]["tmp_name"];
-        $folder = "image/$file_name";
-        echo "<p>$file_name<p>";
-
-    }
-    ?>
   </body>
 </html>
