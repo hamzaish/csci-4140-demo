4ff288676efa2dc93169121468ef165a9486fae7
Hamza Ishaque <hamzaish@ad.unc.edu>
2024-02-20 18:08:42 +0700

    Update edit-photo.php

--- web/edit-photo.php
+++ web/edit-photo.php
@@ -1,6 +1,5 @@
 <?php
     $connect = include('db_connect.php');
-    header('Content-Type: image/jpeg');
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +12,7 @@
         $folder = SITE_ROOT."/";
         $target = $folder.$file_name;
         move_uploaded_file($temp_name, $target);
-        $image = new Imagick($file_name);
+        $image = new Imagick('image.jpg');
         $image->setResolution(300, 300);
         $image->setImageFormat("jpg");
         ob_start();
